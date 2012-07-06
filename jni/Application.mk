5bd35c33b5e708df96969872c5d4d67a95a7368d
Ben Dodson <bjdodson@Bens-MacBook-Pro.local>
2012-07-06 16:04:32 -0700

    Support 20/30 mode from Java; fix for uninitialized variable

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,6 +1,6 @@
 # ARMv7 is significanly faster due to the use of the hardware FPU
 APP_STL := gnustl_static
 APP_ABI := armeabi armeabi-v7a
-APP_OPTIM := release
-APP_BUILD_SCRIPT := /Users/lukeweber/iLBC-android-wrapper/jni/Android.mk
+APP_OPTIM := debug #release
+#APP_BUILD_SCRIPT := /Users/lukeweber/iLBC-android-wrapper/jni/Android.mk
 # APP_CPPFLAGS += -fexceptions -frtti
